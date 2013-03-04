b50dbeb9d71de47971ee3396123d5cb3cdc19c0d
James Firth <hello@james-firth.com>
2013-03-04 00:26:19 -0600

    Added IP fetching
    Pythonified my IP lookup

--- raspberry-pibullet.py
+++ raspberry-pibullet.py
@@ -2,7 +2,12 @@ import socket
 import sys
 import pushbullet
 from pushbullet import PushBullet
+from socket import socket, SOCK_DGRAM, AF_INET 
 
+#getting IP address
+s = socket(AF_INET, SOCK_DGRAM) 
+s.connect(('google.com', 0))#using Google since it's pretty well always reachable
+IP = s.getsockname()[0] #grab IP portion
 
 #Make sure to put your api key here to work!
 apiKey = "YOUR_API_KEY_HERE"
@@ -10,7 +15,6 @@ apiKey = "YOUR_API_KEY_HERE"
 #create pushbullet obj and get list of devices
 pb = PushBullet(apiKey)
 devices = pb.getDevices()
-IP = sys.argv[1]
 
 #push IP to device!
 pb.pushNote(devices[0]["id"], "IP Address", IP)
